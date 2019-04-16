412eda57b576329f2f4a9c6684ea091de043e31c
Jessica Perina <jperina@gmail.com>
2019-04-15 19:22:03 -0700

    refactor: replace header

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from '../Header/Header'
 import MasterGame from '../MasterGame/MasterGame';
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="App">
+        <header>
+          <Header />
         </header>
         <div className="cyan darken-4">
           <MasterGame />
