8710020fcc55e7f955cc9f514ef2c54742056dc1
Jessica Perina <jperina@gmail.com>
2019-04-15 19:19:19 -0700

    feat(header): add header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#!" className="brand-logo center">Tic-Tac-Toe</a>
+        <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">info_outline</i></a>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
